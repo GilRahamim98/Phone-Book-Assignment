c9e7432106153ed368e2651186e57ad599cd00b9
Gil <frogvsdogvsmarsmelo@gmail.com>
2022-11-13 21:54:28 +0200

    Start graphQL front

--- front/src/Components/ContactInList.js
+++ front/src/Components/ContactInList.js
@@ -4,7 +4,7 @@ import './ContactInList.css'
 function ContactInList(contact) {
 
     return (
-        <div >{contact.nickname ? <h1>{contact.nickname}</h1> : <div><h1>{contact.first_name} {contact.last_name} <img className="contact_photo" src={contact.photo} alt={contact.first_name} style={{ display: "inline-block" }} ></img></h1> </div>}</div >
+        <div >{contact.nickname ? <h1>{contact.nickname}</h1> : <div><h1>{contact.firstName} {contact.lastName} <img className="contact_photo" src={contact.photo} alt={contact.first_name} style={{ display: "inline-block" }} ></img></h1> </div>}</div >
     )
 }
 
--- front/src/index.js
+++ front/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
